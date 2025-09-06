fe7fc492218bc244bf25e5a8c515bf06c15cc15b
InuDeveloper <daviddevlife@gmail.com>
2025-09-06 17:53:45 -0400

    Test program made, need to specify the files - Working in an UI

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-from pdf2docx import Converter
+from pdf2docx import Converter 
+# We import the library that will help us convert the PDF file to a docx data type file.
 
-def pdf_to_docx(pdf_file, docx_file):
+def pdf_to_docx(pdf_file, docx_file): # Defining the attributes pdf_file and docx_file
    # Create the PDF converter object
    cv = Converter(pdf_file)
 
@@ -8,3 +9,6 @@ def pdf_to_docx(pdf_file, docx_file):
    cv.convert(docx_file, start=0, end=None) # You can specify page range from start/end
    cv.close()
    print(f"Converted {pdf_file} to {docx_file}")
+
+# Test usage - I'm going to give an UI later.
+pdf_to_docx("Introduccion a la algoritmia - Diagramas de flujo, tarea.pdf", "Introduccion a la algoritmia - Diagramas de flujo, tarea.docx")
